425dc8cdfdce316560fd2e647b1ed54e62e54d1c
warishasan <waris.hasan00@gmail.com>
2020-11-07 17:55:27 +0500

    removed unnecessary comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,8 +34,7 @@ export default function Home({ location }) {
   let stripePromise
   const getStripe = () => {
     if (!stripePromise) {
-    stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
-   //  stripePromise = loadStripe("pk_test_51HjkoeGyQCuilANZtIwOicVLWKtEKGTeDq1YISWnmd1d2MIdUqxLLWkmAkgsYCsPT12ExruGRLCEWQ8XgaSNrtut00egD3xTxQ")
+      stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
     }
     return stripePromise
   }
